589ac5a574cd6b78825f4f4e6841fd28a55e1561
Alexa <alexandraws29@outlook.com>
2021-01-21 21:05:08 -0700

    reinstalled dependencies, added provider to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-
+import { Provider } from 'react-redux';
+import store from './utils/store';
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
@@ -28,6 +29,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
+        <Provider store={store}>
           <Nav />
           <Switch>
             <Route exact path="/" component={Home} />
@@ -37,6 +39,7 @@ function App() {
             <Route exact path="/products/:id" component={Detail} />
             <Route component={NoMatch} />
           </Switch>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
